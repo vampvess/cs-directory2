247dd2b84ce1568bcc200b519229cea1f246d677
vampvess <vampvess@gmail.com>
2016-12-31 00:03:31 +0800

    running app

---
+++ directory/www/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Person
+
+def index(request):
+    people = Person.objects.all()
+    return render(request, 'index.html', {'people':people})
+# Create your views here.
